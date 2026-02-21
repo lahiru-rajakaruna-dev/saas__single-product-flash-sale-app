59af675e69545472346e6ea47d2a659f5109f7bd
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-21 12:38:04 +0530

    add random image api host

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    reactCompiler: true,
+    images       : {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+                port    : '',
+                // href:'https://picsum.photos/'
+            }
+        ]
+    },
 };
 
 export default nextConfig;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import {
+    clsx,
+    type ClassValue
+}                  from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
 }
+
+
+export function keyGenerator(...values: string[]) {
+    return `${ Math.floor(Math.random() * 1_000_000_000) }-` + values.join('-')
+}
+
+
+export type Prettify<T> = {
+                              [k in keyof T]: T[k];
+                          } & {}
