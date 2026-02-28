3633f88721025d7a3718552ad0a6a2d291a639c8
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-28 15:59:25 +0530

    refactor(orm module): fixed bloated BaseOrmService

--- src/app/api/_modules/orm_module/abstract.orm.ts
+++ src/app/api/_modules/orm_module/abstract.orm.ts
@@ -1,84 +1,27 @@
-import {ILoggerService} from "@/app/api/_modules/logger_module/logger.interface";
-import {IOrmService}    from "@/app/api/_modules/orm_module/orm.interface";
-import {
-	TInsertPage,
-	TInsertProduct,
-	TInsertUser,
-	TSelectPage,
-	TSelectProduct,
-	TSelectUser,
-	TUpdatePage,
-	TUpdateProduct,
-	TUpdateUser
-}                       from "@drizzle/schema";
+import {LoggerServiceFactory} from "@/app/api/_modules/logger_module/logger.factory";
+import {ILoggerService}       from "@/app/api/_modules/logger_module/logger.interface";
+import {IOrmService}          from "@/app/api/_modules/orm_module/orm.interface";
 
 
 
-export abstract class BaseOrmService implements IOrmService {
-	protected static instance: IOrmService
-	protected readonly logger: ILoggerService
+export abstract class BaseOrmService<TDriver>
+	implements IOrmService<TDriver> {
+	private readonly _logger: ILoggerService
+	private readonly _driver: TDriver;
 
-	protected constructor(logger: ILoggerService) {
-		this.logger = logger
+	protected constructor(
+		dbDriver: TDriver,
+	) {
+		this._logger = LoggerServiceFactory.getLogger()
+		this._driver = dbDriver
 	}
 
-	abstract createPage(
-		userID: string,
-		page: TInsertPage
-	): Promise<TSelectPage>
+	get driver(): TDriver {
+		return this._driver;
+	}
 
-	abstract createProduct(
-		userID: string,
-		product: TInsertProduct
-	): Promise<TSelectProduct>
-
-	abstract createUser(user: TInsertUser): Promise<TSelectUser>
-
-	abstract deletePageById(
-		userID: string,
-		id: string
-	): Promise<boolean>
-
-	abstract deleteProductById(
-		userID: string,
-		id: string
-	): Promise<boolean>
-
-	abstract deleteUserById(id: string): Promise<boolean>
-
-	abstract getPageById(
-		userID: string,
-		id: string
-	): Promise<TSelectPage | undefined>
-
-	abstract getPagesByUserId(userId: string): Promise<TSelectPage[]>
-
-	abstract getProductById(
-		userID: string,
-		id: string
-	): Promise<TSelectProduct | undefined>
-
-	abstract getProductsByUserId(userId: string): Promise<TSelectProduct[]>
-
-	abstract getUserById(id: string): Promise<TSelectUser | undefined>
-
-	abstract getUsers(): Promise<TSelectUser[]>
-
-	abstract updatePageById(
-		userID: string,
-		id: string,
-		pageUpdates: TUpdatePage
-	): Promise<TSelectPage>
-
-	abstract updateProductById(
-		userID: string,
-		id: string,
-		productUpdates: TUpdateProduct
-	): Promise<TSelectProduct>
-
-	abstract updateUserById(
-		id: string,
-		userUpdates: TUpdateUser
-	): Promise<TSelectUser>
+	get logger(): ILoggerService {
+		return this._logger
+	}
 
 }
