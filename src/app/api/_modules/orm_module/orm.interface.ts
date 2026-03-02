0bfb56b855ad6c2f2cd2ede8da6d5ff170048cf8
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-02 23:03:38 +0530

    refactor(orm module): split into multiple interfaces

--- src/app/api/_modules/orm_module/orm.interface.ts
+++ src/app/api/_modules/orm_module/orm.interface.ts
@@ -1,14 +1,74 @@
-import {ILoggerService} from "@/app/api/_modules/logger_module/logger.interface";
-import {TDrizzleOrm}    from "@/app/api/_modules/orm_module/drizzle.orm";
+import {TDrizzleOrm} from "@/app/api/_modules/orm_module/drizzle.orm";
+import {
+	TInsertPage,
+	TInsertProduct,
+	TInsertUser,
+	TSelectPage,
+	TSelectProduct,
+	TSelectUser,
+	TUpdatePage,
+	TUpdateProduct,
+	TUpdateUser
+}                    from "@drizzle/schema";
 
 
 
-export interface IOrmService<TDriver> {
+export interface IOrmService {
+}
 
-	get driver(): TDriver
 
-	get logger(): ILoggerService
 
+export interface IOrmUserManagable extends IOrmService {
+	createUser(data: TInsertUser): Promise<TSelectUser>
+
+	updateUser(
+		userID: string,
+		updates: TUpdateUser
+	): Promise<TSelectUser>
+
+	getUser(userID: string): Promise<TSelectUser | undefined>
+
+	getUsers(adminID: string): Promise<TSelectUser[]>
+}
+
+
+
+export interface IOrmProductManagable extends IOrmService {
+
+	createProduct(data: TInsertProduct): Promise<TSelectProduct>
+
+	updateProduct(
+		userID: string,
+		productID: string,
+		updates: TUpdateProduct
+	): Promise<TSelectProduct>
+
+	getProduct(
+		userID: string,
+		productID: string
+	): Promise<TSelectProduct | undefined>
+
+	getProducts(userID: string): Promise<TSelectProduct[]>
+
+	deleteProduct(userID: string, productID: string): Promise<boolean>
+}
+
+
+
+export interface IOrmPageManagable extends IOrmService {
+	createPage(data: TInsertPage): Promise<TInsertPage>
+
+	updatePage(
+		userID: string,
+		pageID: string,
+		updates: TUpdatePage
+	): Promise<TSelectPage>
+
+	getPage(userID: string, pageID: string): Promise<TSelectPage | undefined>
+
+	getPages(userID: string): Promise<TSelectPage[]>
+
+	deletePage(userID: string, pageID: string): Promise<boolean>
 }
 
 
